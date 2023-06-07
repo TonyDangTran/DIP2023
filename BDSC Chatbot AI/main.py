96645ba2e71610c214ff79d64ab93ba09e11fd12
TonyDangTran <tonyisamacaroni@gmail.com>
2023-06-07 13:19:17 +1200

    starting the GUI for the chatbot.

---
+++ BDSC Chatbot AI/main.py
@@ -0,0 +1,10 @@
+from tkinter import*
+from tkinter import ttk
+
+window = Tk()
+window.geometry("500x1000")
+window.title("BDSC Chatbot AI")
+frame = Frame(window)
+frame.pack()
+
+window.mainloop()
